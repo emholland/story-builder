ff618201a6cc760f42ffff4bf283f614e3b1fcc1
csimmons2024 <tul13214@temple.edu>
2025-02-25 11:31:28 -0500

    Created Home page

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AgentScreen from './assets/AgentScreen.jsx'
+import Home from './assets/Home.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AgentScreen />
+    <AppScreen />
   </StrictMode>,
 )
