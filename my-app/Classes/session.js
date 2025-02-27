64fd6e79c126c10926fd804a7c04f6790bd86b4c
Elisa Holland <102695327+emholland@users.noreply.github.com>
2025-02-27 14:46:49 -0500

    session class

---
+++ my-app/Classes/session.js
@@ -0,0 +1,27 @@
+class Session {
+    // Constructor
+    constructor(user, story, prompt) {
+      this.user = user;
+      this.Story = story;
+      this.prompt;
+      this.numberOfChapters; 
+      this.currentChapters = [];
+      this.Agents = [];
+      // ...
+    }
+  
+    //Elisa will do this Method
+    createAgent() {
+      // Code for method1
+    }
+  
+    method2(parameter) {
+      // Code for method2
+    }
+  
+    // Static method
+    static staticMethod() {
+      // Code for static method
+    }
+  }
+  
