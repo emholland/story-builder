6aeb7ef1e5d5ca68a114b176bfcba530ebe017f2
Chiku Okechukwu <chikunwenyi@gmail.com>
2025-03-08 01:00:24 -0500

    firebase connection added, will add keys to the documents

--- my-app/firebase.js
+++ my-app/firebase.js
@@ -1,22 +1,20 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6ed2mMisJetf4l96KrhtUP6sS_OLRG3U",
-  authDomain: "storybuilder-153f1.firebaseapp.com",
-  databaseURL: "https://storybuilder-153f1-default-rtdb.firebaseio.com",
-  projectId: "storybuilder-153f1",
-  storageBucket: "storybuilder-153f1.firebasestorage.app",
-  messagingSenderId: "1061530797877",
-  appId: "1:1061530797877:web:ec4e062f5f9484775f2e7c",
-  measurementId: "G-N5DMY71FPS"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app, analytics };
