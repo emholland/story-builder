fbaca4034f8efa7a058512fa0a1fa3f7c5c12cd7
Chiku Okechukwu <chikunwenyi@gmail.com>
2025-03-10 16:57:27 -0400

    adding front end routing and a selection

---
+++ my-app/src/AgentGeneration.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function AgentGeneration() {
+
+    return (
+        <div>
+            placeholder
+        </div>
+    )
+
+}
+
+export default AgentGeneration;
