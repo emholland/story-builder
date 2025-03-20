72db209f97330a0ead2d9852bd767dba29693557
csimmons2024 <tul13214@temple.edu>
2025-03-20 15:08:03 -0400

    Story Creation Page & Style

---
+++ my-app/src/Pages/StoryCreationPage/StoryCreationPage.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./StoryCreationPage.css";
+
+ {/*
+     Black Board: width: 1338.0001220703125;
+                 height: 979.0703125;
+                 top: 67.93px;
+                 left: 245px;
+                 border-radius: 25.34px;
+
+     AgentTextBox: width: 962.1573486328125;
+                   height: 347.6545104980469;
+                   top: 229.54px;
+                   left: 450.77px;
+                   border-radius: 11.28px;
+                   border-width: 0.94px;
+                   background: #FFFFFF0D;
+
+     PhaseBox: width: 638;
+               height: 78;
+               top: 101px;
+               left: 680px;
+               border-radius: 12px;
+
+     User Rectangle: width: 216;
+                     height: 56;
+                     top: 32px;
+                     left: 28px;
+                     border-radius: 8px;
+                     padding: 12px;
+                     gap: 12px;
+
+     AddAgents: width: 216;
+                height: 42;
+                top: 123px;
+                left: 23px;
+                border-radius: 8px;
+                padding: 12px;
+                gap: 6px;
+                background: #222222;
+
+     PickAgentError: width: 364.56744384765625;
+                     height: 84.56461334228516;
+                     top: 704.98px;
+                     left: 731.72px;
+                     border-radius: 18.79px;
+                      background: #97181880;
+
+
+     */}
+function StoryCreation () {
+    return (
+        );
+    }
+export default StoryCreation;
