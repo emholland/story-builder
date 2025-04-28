e2d71a9a5d5b47ef021272489ee09a8d734b78f6
Elisa Holland <102695327+emholland@users.noreply.github.com>
2025-04-28 07:15:14 -0400

    nav

--- my-app/src/Pages/FinalStoryPage/FinalStoryPage.jsx
+++ my-app/src/Pages/FinalStoryPage/FinalStoryPage.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import "./FinalStoryPage.css";
+import { useNavigate } from "react-router-dom";
 
 
 
 function FinalStoryPage() {
     const location = useLocation();
     const finalStory = location.state?.finalStory;
+    const navigate = useNavigate();
+
     
     return (
          <div className="final-story-container">
+          <button onClick={() => navigate(`/`)} className="home-button">
+            Home
+          </button>
         <h1>Your Final Story!</h1>
         <div className="story-box">
         {finalStory?.split("\n\n").map((paragraph, index) => {
